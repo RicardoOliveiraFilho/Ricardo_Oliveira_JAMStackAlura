e33b09c81ae3d05c58c8f3c48d3249c0d901fa1e
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2021-03-14 13:18:51 -0300

    Setup de Projetos like a PRO!

---
+++ script-test.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+const shell = require('shelljs');
+
+console.log('Olá Mundo');
+
+const resultado = shell.exec('git diff --name-only adicao_modal..main', { silent: true });
+
+console.log(resultado.stdout.split('\n'));
