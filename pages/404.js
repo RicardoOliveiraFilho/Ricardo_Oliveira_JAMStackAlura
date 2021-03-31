cc14c2b802baf270c18c1e73a0da856d6d477c4a
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2021-03-31 13:39:47 -0300

    feat(construção página 404): implementação Finalizada da Página 404

--- pages/404.js
+++ pages/404.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 
 import Box from '../src/components/foundation/layout/Box';
+import Grid from '../src/components/foundation/layout/Grid';
 import Header from '../src/components/commons/Header';
 import Text from '../src/components/foundation/Text';
 import Image from '../src/components/commons/Image';
@@ -18,15 +19,11 @@ const AreaText404 = styled.div`
   align-self: center;
   border: 1px solid ${({ theme }) => theme.colors.background.primary.color};
   background-color: ${({ theme }) => theme.colors.primary.main.contrastColor};
-  margin-top: 217px;
-  margin-bottom: 273px;
   width: 282px;
   height: 222px;
 
   ${breakpointsMedia({
     md: css`
-      margin-top: 290px;
-      margin-bottom: 314px;
       width: 415px;
       height: 254px;
 
@@ -47,72 +44,140 @@ export default function Page404() {
       flex="1"
       display="flex"
       flexDirection="column"
+      padding="0px"
+      overflowX="hidden"
     >
       <Header />
-      <AreaText404>
-        <Text
-          tag="h1"
-          variant="titlePage404XS"
-          margin="0px"
+      <Grid.Container
+        flex="1"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        padding="0px"
+      >
+        <Box
+          backgroundImage="url(/images/mandala.png)"
+          backgroundRepeat="no-repeat"
+          backgroundPosition="center center"
+          padding="20px"
+          width={{
+            md: '100%',
+          }}
+          height={{
+            md: '100%',
+          }}
+          marginTop="25px"
         >
-          404
-        </Text>
-        <Text
-          tag="h3"
-          variant="textPage404XS"
-          margin="0px"
-        >
-          Página não encontrada
-        </Text>
-      </AreaText404>
-      <Image
-        src="/images/planta.png"
-        alt="Planta"
-        width={{
-          xs: 381,
-          md: 381,
-        }}
-        height={{
-          xs: 571,
-          md: 571,
-        }}
-        position="absolute"
-        zIndex="-10"
-        opacity=".5"
-        transform="rotate(490deg) matrix(0.64, 0.77, 0.77, -0.64, 0, 0)"
-        marginTop={{
-          xs: '125px',
-          md: '230px',
-        }}
-        marginLeft={{
-          xs: '-180px',
-          md: '235px',
-        }}
-      />
-      <Image
-        src="/images/planta.png"
-        alt="Planta"
-        width={{
-          xs: 381,
-          md: 381,
-        }}
-        height={{
-          xs: 571,
-          md: 571,
-        }}
-        position="absolute"
-        zIndex="-10"
-        opacity=".5"
-        transform="rotate(590deg) rotateY(180deg) matrix(0.64, 0.77, 0.77, -0.64, 0, 0)"
-        marginTop={{
-          xs: '215px',
-          md: '230px',
-        }}
-        marginLeft={{
-          xs: '240px',
-          md: '627px',
-        }}
-      />
+          <Grid.Row
+            flex="1"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid.Col
+              flex="1"
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              offset={{
+                xs: 0,
+                md: 0,
+              }}
+              value={{
+                xs: 1,
+                md: 5,
+              }}
+            >
+              <Image
+                src="/images/planta.png"
+                alt="Planta"
+                width={{
+                  xs: 381,
+                  md: 381,
+                }}
+                height={{
+                  xs: 571,
+                  md: 571,
+                }}
+                zIndex="-10"
+                opacity=".5"
+                transform="scaleX(-1)"
+                marginTop={{
+                  xs: '90px',
+                  md: '65px',
+                }}
+                marginLeft={{
+                  xs: '0px',
+                  md: '40px',
+                }}
+              />
+            </Grid.Col>
+            <Grid.Col
+              flex="1"
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              value={{
+                md: 2,
+              }}
+            >
+              <AreaText404>
+                <Text
+                  tag="h1"
+                  variant="titlePage404XS"
+                  margin="0px"
+                >
+                  404
+                </Text>
+                <Text
+                  tag="h3"
+                  variant="textPage404XS"
+                  margin="0px"
+                >
+                  Página não encontrada
+                </Text>
+              </AreaText404>
+            </Grid.Col>
+            <Grid.Col
+              flex="1"
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              value={{
+                xs: 1,
+                md: 5,
+              }}
+            >
+              <Image
+                src="/images/planta.png"
+                alt="Planta"
+                width={{
+                  xs: 381,
+                  md: 381,
+                }}
+                height={{
+                  xs: 571,
+                  md: 571,
+                }}
+                zIndex="-10"
+                opacity=".5"
+                transform="scaleX(1)"
+                marginTop={{
+                  xs: '90px',
+                  md: '65px',
+                }}
+                marginRight={{
+                  md: '40px',
+                }}
+              />
+            </Grid.Col>
+          </Grid.Row>
+        </Box>
+      </Grid.Container>
       <Footer />
     </Box>
   );
